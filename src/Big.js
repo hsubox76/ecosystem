1670794a6c6fc4ada4ed7ce0ee54494d572f6414
Christina Holland <hsubox@gmail.com>
2018-03-19 15:11:14 -0700

    mess around with state control

--- src/Big.js
+++ src/Big.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const COLORS = [
+  [255, 0, 0, 255],
+  [0, 255, 0, 255],
+  [0, 0, 255, 255]
+];
+
 const SIZE = 10;
 class Big extends React.Component {
   static propTypes = {
@@ -14,19 +20,24 @@ class Big extends React.Component {
     super(props);
     this.state = {
       position: props.initialPosition,
-      direction: 20,
+      angle: 20,
       speed: 1
     };
   }
   componentDidMount() {
-    window.requestAnimationFrame(this.step);
+    // window.requestAnimationFrame(this.step);
     const { x: cX, y: cY } = this.state.position;
-    this.props.moveAgent({ index: this.props.index, cX, cY, direction: this.state.direction, width: SIZE, height: SIZE, color: [255, 150, 150, 255] });
+    this.props.moveAgent({ index: this.props.index, cX, cY, angle: this.state.angle, width: SIZE, height: SIZE, color: [255, 150, 150, 255] });
   }
-  step = () => {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.frame !== this.props.frame) {
+      this.move();
+    }
+  }
+  move = () => {
     const { x: cX, y: cY } = this.state.position;
-    const xSpeed = this.state.speed * Math.sin(this.state.direction);
-    const ySpeed = this.state.speed * Math.cos(this.state.direction);
+    const xSpeed = this.state.speed * Math.sin(this.state.angle);
+    const ySpeed = this.state.speed * Math.cos(this.state.angle);
     const nextX = Math.min(cX + xSpeed, 60);
     const nextY = Math.min(cY + ySpeed, 60);
     if (nextX !== cX || nextY !== cY) {
@@ -36,9 +47,19 @@ class Big extends React.Component {
           y: nextY
         })
       });
-      this.props.moveAgent({ index: this.props.index, cX: nextX, cY: nextY, direction: this.state.direction, width: SIZE, height: SIZE, color: [255, 150, 150, 255] });
     }
-    window.requestAnimationFrame(this.step);
+    // With current setup, an agent will disappear if it doesn't report state every frame
+    // Maybe this can be changed later
+    this.props.moveAgent({
+      index: this.props.index,
+      cX: nextX,
+      cY: nextY,
+      angle: this.state.angle,
+      width: SIZE,
+      height: SIZE,
+      color: COLORS[this.props.index]
+    });
+    // window.requestAnimationFrame(this.step);
   };
   render() {
     return null;
